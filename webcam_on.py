2b68c3577043f8b3efecdc60507be17e18cb3d1f
icequeen3104 <154549622+icequeen3104@users.noreply.github.com>
2023-12-22 20:32:23 +0000

    Update webcam_on.py

--- webcam_on.py
+++ webcam_on.py
@@ -1,9 +1,9 @@
 import cv2 as cv
 capture = cv.VideoCapture(0)               #captures your PC's webcam
-capture.set(3,640)
-capture.set(4,480)
-capture.set(10,100)
-true = 1
+capture.set(3,640)                         #sets the height of the image
+capture.set(4,480)                         #sets the width of the image
+capture.set(10,100)                        #for brightening the image
+true = 1                                   
 while true:
     istrue, frame = capture.read()         #this reads the captured video frame by frame
     cv.imshow('Video', frame)              #displays each frame of the video
