bde259011228a8e93ddd14b2751267e1455bfdb1
icequeen3104 <154549622+icequeen3104@users.noreply.github.com>
2023-12-30 20:44:57 +0000

    Updated the  code

--- matplotlib/bar_chart.py
+++ matplotlib/bar_chart.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt                                          #import
 import pandas as pd                                                      #importing pandas to read given csv file 
 plt.style.use('bmh')                                                     #for styling the plot
 df = pd.read_csv('FILE PATH.csv')                                        #'df' stands for dataframe which stores the csv file and reads it
-x = df['x']                                                              #here x stores the set of values given in the file
-y = df['y']                                                              #here y stores another set of values given in the file
+x = df['']                                                               #here x stores the set of values given in the file
+y = df['']                                                               #here y stores another set of values given in the file
 plt.xlabel('')                                                           #this labels the x-axis of the plot 
 plt.ylabel('')                                                           #this labels the y-axis of the plot
 plt.bar(x,y, color = "g", align = "edge", edgecolor = "r")               #this forms the customised bar chart
