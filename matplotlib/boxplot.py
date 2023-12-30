c4338549498698321d3cdb3aa0dee8504e589131
icequeen3104 <154549622+icequeen3104@users.noreply.github.com>
2023-12-30 20:45:50 +0000

    Updated the code

--- matplotlib/boxplot.py
+++ matplotlib/boxplot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt                                   #importing the
 import pandas as pd                                               #importing pandas to read given csv file 
 plt.style.use('bmh')                                              #for styling the plot
 df = pd.read_csv('FILE PATH.csv')                                 #'df' stands for dataframe which stores the csv file and reads it
-x = df['x']                                                       #this stores the given set of values
+x = df['']                                                        #this stores the given set of values
 plt.boxplot(x)                                                    #this forms the box plot of the x values
 plt.ylabel('')                                                    #this labels the y-axis as the boxplot will be read form y-axis only
 plt.title("BOX PLOT of MEAN values")                              #title for the plot
