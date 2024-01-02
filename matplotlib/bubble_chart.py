f5fa01cec6bc7217ff2226027f1b2ac1b3c9f749
icequeen3104 <154549622+icequeen3104@users.noreply.github.com>
2024-01-02 21:04:35 +0530

    Updated and renamed

--- matplotlib/bubble_chart.py
+++ matplotlib/bubble_chart.py
@@ -6,8 +6,8 @@ x = df['']                                                               #here x
 y = df['']                                                               #here y stores another set of values given in the file
 plt.xlabel('')                                                           #this labels the x-axis of the plot 
 plt.ylabel('')                                                           #this labels the y-axis of the plot
-plt.bar(x,y, color = "g", align = "edge", edgecolor = "r")               #this forms the customised bar chart
-plt.title("BAR CHART")                                                   #sets title according to your requirement
+plt.scatter(x,y, color = "purple", alpha = 0.3)                          #this forms the customised bubble chart
+plt.title("BUBBLE CHART")                                                #sets title according to your requirement
 plt.show()                                                               #displays the output
 plt.savefig("FILE NAME.png")                                             #saves your output in 'png' format
 plt.tight_layout()                                                       #structures everything and saves frome overlapping value plots
