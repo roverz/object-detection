1a3b5d9079747644f2ef0a89a624a49dafa38730
Santanu Paik <127647619+itnerd7@users.noreply.github.com>
2024-04-08 21:28:56 +0000

    Added video stream code

---
+++ raspberry-pi/stream_video.py
@@ -0,0 +1,24 @@
+from flask import Flask, Response
+from picamera2 import Picamera2
+import cv2
+
+app = Flask(__name__)
+
+camera = Picamera2()
+camera.configure(camera.create_preview_configuration(main={"format": 'XRGB8888', "size": (1296,972)}))
+camera.start()
+
+def generate_frames():
+    while True:
+        frame = camera.capture_array()
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
